11ab04583cc44cb3ff0b0bce2f52899fc4559890
Max Pöttke <maxotar2003@gmail.com>
2025-10-28 12:34:02 +0100

    Graybox and character setup

---
+++ Assets/TriggerEventer.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventer : MonoBehaviour
+{
+    public string tag = "";
+    public UnityEvent onTriggerEnter;
+    public UnityEvent onTriggerStay;
+    public UnityEvent onTriggerExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(tag == ""){
+            onTriggerEnter?.Invoke();
+        }
+        else if(other.CompareTag(tag)){
+            onTriggerEnter?.Invoke();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(tag == ""){
+            onTriggerStay?.Invoke();
+        }
+        else if(other.CompareTag(tag)){
+            onTriggerStay?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(tag == ""){
+            onTriggerExit?.Invoke();
+        }
+        else if(other.CompareTag(tag)){
+            onTriggerExit?.Invoke();
+        }
+    }
+}
